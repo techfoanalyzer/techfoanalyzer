d15935b48e9a1f3a564e8207e0ac2050c745defb
techfoanalyzer <shahbazchadhar5341@gmail.com>
2026-08-10 12:25:32 +0500

    robots

--- client/src/app/robots.js
+++ client/src/app/robots.js
@@ -1,25 +1,25 @@
-// app/robots.js
-
 export default function robots() {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://www.techfoanalyzer.com";
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://techfoanalyzer.com";
 
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: [
-        "/blog",           
-        "/blog/",          
-        "/category/",      
-        "/search",        
-        "/users/", 
-        "/comments/",
-        "/sign-in", 
-        "/sign-up", 
-        "/profile", 
-        "/forget-password",
-      ],
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          "/blog$",          
+          "/blog/*",         
+          "/category/", 
+          "/search", 
+          "/users/", 
+          "/comments/",
+          "/sign-in", 
+          "/sign-up", 
+          "/profile", 
+          "/forget-password",
+        ],
+      },
+    ],
     sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
